c2a785a7542f210bd1d8d0e95993b2d10fd34086
Robert Engler <rengler33@gmail.com>
2021-12-15 23:45:46 -0600

    Reformat

--- work/hangman/lib/impl/game.ex
+++ work/hangman/lib/impl/game.ex
@@ -1,17 +1,16 @@
 defmodule Hangman.Impl.Game do
-
   @type t :: %__MODULE__{
-    turns_left: integer,
-    game_state: Hangman.state,
-    letters: list(String.t),
-    used: MapSet.t(String.t),
-  }
+          turns_left: integer,
+          game_state: Hangman.state(),
+          letters: list(String.t()),
+          used: MapSet.t(String.t())
+        }
 
   defstruct(
     turns_left: 7,
     game_state: :initializing,
     letters: [],
-    used: MapSet.new(),
+    used: MapSet.new()
   )
 
   def new_game do
@@ -19,5 +18,4 @@ defmodule Hangman.Impl.Game do
       letters: Dictionary.random_word() |> String.codepoints()
     }
   end
-
 end
