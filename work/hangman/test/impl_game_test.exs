faff238f2d0ff93088db1b06167b92a64aa84fec
Robert Engler <rengler33@gmail.com>
2021-12-18 10:40:40 -0600

    Update impl so word can be specified

--- work/hangman/test/impl_game_test.exs
+++ work/hangman/test/impl_game_test.exs
@@ -10,4 +10,11 @@ defmodule HangmanImplGameTest do
     assert game.game_state == :initializing
     assert length(game.letters) > 0
   end
+
+  test "new game returns correct word" do
+    game = Game.new_game("wombat")
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert game.letters == ["w", "o", "m", "b", "a", "t"]
+  end
 end
